5a06dd7ed90a1bf9151c22074591bcdc074aa07e
kyucchoi <kyucchoi@naver.com>
2024-06-11 14:11:18 +0900

    240611 유저 프로필 컴포넌트 구현

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-    <nav>
-        <RouterLink to="/">Home</RouterLink>
-    </nav>
-    <RouterView />
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <HomePage />
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import HomePage from '@/pages/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
